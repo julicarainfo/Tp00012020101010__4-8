7fd603c1a60d800a5e183a5b51072f942bf68edb
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-04 11:55:57 -0300

    anda el log-in, falta el sign-up

---
+++ back/index.js
@@ -0,0 +1,24 @@
+import  express from "express";
+import cors from "cors";
+var app = express()
+const port = 5000;
+app.use(express.json());
+app.use(cors())
+
+let usuario="papaletas";
+let contraseña = "papalaletasaletas";
+
+
+
+app.post('/login',async(req,res)=>{
+    let usuarioIngresado = req.body.usuario
+    let contraseñaIngresada = req.body.contraseña
+    if (usuarioIngresado === usuario  && contraseñaIngresada === contraseña) {
+        res.status(200).send(" pudiste iniciar sesion correctament");
+	} else {
+		res.status(400).send("Usuario o contraseña invalidos");
+	}
+})
+app.listen(port,() =>{
+    console.log(`Example app listening on port ${port}`)
+})
